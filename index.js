9d9df638e596834f59571759a01918a37c852a10
Schmidt-Aaron <aschmidt808@gmail.com>
2018-04-09 10:58:37 -0700

    get polls on page load

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const routes = require('./routes/voting');
 
+//static files
+app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/views'))
+
 //middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //route root
 app.get('/', (req, res) => {
-  res.send("heelo root!")
+  res.sendFile("index.html")
 })
 
 //API routes
---
+++ public/app.js
@@ -0,0 +1,31 @@
+const pollList = document.querySelector('.polls');
+
+//get all the polls
+const getPolls = () => {
+  axios.get('/api')
+    .then((data) => {
+      parsePolls(data.data)
+    })
+    .catch(err => console.log(err));
+}
+
+//parse poll data
+const parsePolls = (polls) => {
+  polls.forEach(poll => postPoll(poll));
+}
+
+//post single poll to page
+const postPoll = (poll) => {
+  console.log(poll)
+  const listItem = document.createElement('li');
+  
+  //data-id to api calls
+  listItem.setAttribute('data-id', poll._id);
+  let singlePoll = poll.question;
+  listItem.innerHTML = singlePoll;
+
+  pollList.appendChild(listItem);
+}
+
+//fire our polls function on page load
+window.addEventListener('load', getPolls )
