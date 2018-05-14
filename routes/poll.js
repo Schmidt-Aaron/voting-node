f4696a958e85eec829343f7348f7bec4e2897d6c
schmidt-aaron <aschmidt808@gmail.com>
2018-05-14 14:27:44 -0700

    added chartjs to poll template

--- routes/poll.js
+++ routes/poll.js
@@ -14,13 +14,24 @@ const findPoll = id => {
 
 router.get('/:id', (req, res) => {
   db.Poll.findById(req.params.id).then(poll => {
+
+    let choices = [];
+    let votes = [];
+
+    poll.choices.forEach((item) => {
+      votes.push(item.votes);
+      choices.push(item.choiceText);
+    })
+
     const pollObj = {
       pageTitle: poll.question,
       question: poll.question,
       author: poll.author,
-      choices: poll.choices,
+      choices,
+      votes,
       pollID: req.params.id,
     };
+    console.log(pollObj);
     res.render('poll', pollObj);
   });
 });
