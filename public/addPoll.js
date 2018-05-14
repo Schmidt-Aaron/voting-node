338aa268018b500da7d725872d133be929c6877f
schmidt-aaron <aschmidt808@gmail.com>
2018-05-14 15:20:59 -0700

    limit number of choices per poll to 5

--- public/addPoll.js
+++ public/addPoll.js
@@ -1,24 +1,27 @@
 const additionalChoice = () => {
-  const choiceNum = document.querySelectorAll('.choice').length + 1;
-  const target = document.querySelector('.choices');
+  if (document.querySelectorAll('.choice').length !== 5 ) {
+    const choiceNum = document.querySelectorAll('.choice').length + 1;
+    const target = document.querySelector('.choices');
+  
+    // parent
+    const choiceDiv = document.createElement('div');
+    choiceDiv.classList.add('choice');
+  
+    const choiceLabel = document.createElement('label');
+    choiceLabel.setAttribute('for', `choice${choiceNum}`);
+    choiceLabel.innerHTML = `Choice #${choiceNum}`;
+    choiceDiv.appendChild(choiceLabel);
+  
+    const choiceInput = document.createElement('input');
+    choiceInput.setAttribute('type', 'text');
+    choiceInput.setAttribute('name', `choices[]`);
+    choiceInput.setAttribute('id', `option${choiceNum}`);
+    choiceDiv.appendChild(choiceInput);
+  
+    target.appendChild(choiceDiv);
+  };
 
-  // parent
-  const choiceDiv = document.createElement('div');
-  choiceDiv.classList.add('choice');
-
-  const choiceLabel = document.createElement('label');
-  choiceLabel.setAttribute('for', `choice${choiceNum}`);
-  choiceLabel.innerHTML = `Choice #${choiceNum}`;
-  choiceDiv.appendChild(choiceLabel);
-
-  const choiceInput = document.createElement('input');
-  choiceInput.setAttribute('type', 'text');
-  choiceInput.setAttribute('name', `choices[]`);
-  choiceInput.setAttribute('id', `option${choiceNum}`);
-  choiceDiv.appendChild(choiceInput);
-
-  target.appendChild(choiceDiv);
-};
+  }
 
 const addChoiceElement = document.querySelector('.addChoice');
 addChoiceElement.addEventListener('click', additionalChoice);
