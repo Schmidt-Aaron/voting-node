25c91a82350131936719367092164b95fdcf8ac7
schmidt-aaron <aschmidt808@gmail.com>
2018-10-05 10:00:29 -0700

    misc updates

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// static files
+app.use(express.static(path.join(__dirname, "/public")));
+
+// middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// set up our routes
+app.use("/", routes);
+
+app.listen(port, () => {
+  console.log(`Now running on ${port}`);
+});
