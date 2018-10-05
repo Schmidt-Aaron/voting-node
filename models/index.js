947db55986f5b464b276aad54e83101989130d5f
schmidt-aaron <aschmidt808@gmail.com>
2018-10-05 09:32:48 -0700

    misc cleanup

--- models/index.js
+++ models/index.js
@@ -8,8 +8,4 @@ mongoose.connect("mongodb://localhost:27017/voting-node");
 
 mongoose.Promise = Promise;
 
-<<<<<<< HEAD
 module.exports.Poll = require("./Poll");
-=======
-module.exports.Polls = require('./Polls');
->>>>>>> merging-routes
